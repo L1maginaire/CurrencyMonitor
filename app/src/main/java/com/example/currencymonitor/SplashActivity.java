3f3705cbafe5e03a52bfae4bb01730305dc4d6c0
Roman <l1maginaire00@gmail.com>
2017-12-08 02:43:55 +0200

    Initial commit

---
+++ app/src/main/java/com/example/currencymonitor/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.example.currencymonitor;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+
+public class SplashActivity extends Activity{
+    long time;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(
+                        SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 2000);
+    }
+}
