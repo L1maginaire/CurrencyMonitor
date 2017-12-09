854de97c5902530952d515c6ea1dc99bf06bc564
Roman <l1maginaire00@gmail.com>
2017-12-09 10:27:43 +0200

    spinner_layout

--- app/src/main/java/com/example/currencymonitor/MainActivity.java
+++ app/src/main/java/com/example/currencymonitor/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.currencymonitor;
 
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.preference.PreferenceManager;
 import android.os.Bundle;
@@ -16,8 +19,12 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -54,9 +61,8 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
         setContentView(R.layout.activity_main);
         dbHelper = new CurrencyDBHelper(MainActivity.this);
         mDb = dbHelper.getWritableDatabase();
-        Cursor cursor = mDb.query(TABLE_NAME, null,null, null, null, null, null);
-        boolean abc = cursor.moveToPosition(5);
-
+        Cursor cursor = mDb.query(TABLE_NAME, null, null, null, null, null, null);
+        cursor.moveToPosition(5);
         String abdc = cursor.getString(cursor.getColumnIndex(CURRENCY));
         String s = cursor.getString(cursor.getColumnIndex(COLUMN_USD));
         mDb.close();
@@ -66,6 +72,13 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main, menu);
+
+        MenuItem item = menu.findItem(R.id.mspinner);
+        Spinner spinner = (Spinner) MenuItemCompat.getActionView(item);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.spinner_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
         return true;
     }
 
@@ -90,5 +103,57 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
         super.onDestroy();
         PreferenceManager.getDefaultSharedPreferences(this)
                 .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    private class Holder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        List<String> list;
+        private TextView mTitleTextView;
+        private ImageView mImageView;
+
+        public Holder(View itemView) {
+            super(itemView);
+            itemView.setOnClickListener(this);
+            mTitleTextView = (TextView) itemView.findViewById(R.id.single_item_textview);
+            mImageView = (ImageView) itemView.findViewById(R.id.single_item_imageview);
+        }
+
+        public void bindCrime(List<String> list) {
+            this.list = list;
+            mTitleTextView.setText("S");
+
+        }
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+
+    private class Adapter extends RecyclerView.Adapter<Holder> {
+        private Cursor mCursor;
+        private Context mContext;
+
+        public Adapter(Context context, Cursor cursor) {
+            mContext = context;
+            mCursor = cursor;
+        }
+
+        @Override
+        public Holder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(mContext);
+            View view = inflater.inflate(R.layout.single_item, parent, false);
+            return new Holder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(Holder holder, int position) {
+            String name = mCursor.getString(mCursor.getColumnIndex(COLUMN_EUR));
+            holder.mTitleTextView.setText(name);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mCursor.getCount();
+        }
     }
 }
