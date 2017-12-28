e6d40173d119be822642611fbfb39fe4c15de7af
l1maginaire <l1maginaire00@gmail.com>
2017-12-28 21:52:10 +0200

    adapter_mvp

--- app/src/main/java/com/example/currencymonitor/MainActivity.java
+++ app/src/main/java/com/example/currencymonitor/MainActivity.java
@@ -23,6 +23,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.currencymonitor.data.db.CurrencyDBHelper;
+import com.example.currencymonitor.utils.Adapter;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -112,57 +113,7 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
         PreferenceManager.getDefaultSharedPreferences(this)
                 .unregisterOnSharedPreferenceChangeListener(this);
     }
-
-    private class Holder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        private TextView textView;
-        private ImageView mImageView;
-
-        public Holder(View itemView) {
-            super(itemView);
-            itemView.setOnClickListener(this);
-            textView = (TextView) itemView.findViewById(R.id.result);
-            mImageView = (ImageView) itemView.findViewById(R.id.single_item_imageview);
-        }
-
-        @Override
-        public void onClick(View v) {
-
-        }
-    }
-
-    private class Adapter extends RecyclerView.Adapter<Holder> {
-        private Context mContext;
-        ArrayList<CurrencyData> list;
-
-        public Adapter(Context context, ArrayList<CurrencyData> list) {
-            mContext = context;
-            this.list = list;
-        }
-
-        @Override
-        public Holder onCreateViewHolder(ViewGroup parent, int viewType) {
-            LayoutInflater inflater = LayoutInflater.from(mContext);
-            View view = inflater.inflate(R.layout.single_item, parent, false);
-            return new Holder(view);
-        }
-
-
-        @Override
-        public void onBindViewHolder(Holder holder, final int position) {
-            if (list == null || list.size() == 0)
-                return;
-            double val = (Double) list.get(position).getValue();
-            int res = (Integer) list.get(position).getPic();
-            holder.textView.setText(String.valueOf(val));
-            holder.mImageView.setImageResource(res);
-        }
-
-        @Override
-        public int getItemCount() {
-            return list.size();
-        }
-    }
-
+    
     private void bindData(Cursor c) {
         if (c == null || !c.moveToFirst()) {
             return;
@@ -174,7 +125,7 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
                 CurrencyData data = new CurrencyData();
                 data.setCoefficient((c.getDouble(i)) == 0.0 ? 1.0 : c.getDouble(i));
                 data.setValue(c.getDouble(i) * 1/*//todo*/);
-                data.setPic(myImageList[i-1]);
+                data.setPic(myImageList[i - 1]);
                 list.add(data);
             }
         }
---
+++ app/src/main/java/com/example/currencymonitor/utils/Adapter.java
@@ -0,0 +1,69 @@
+package com.example.currencymonitor.utils;
+
+import android.database.Cursor;
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.currencymonitor.CurrencyData;
+import com.example.currencymonitor.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by l1maginaire on 12/28/17.
+ */
+
+public class Adapter extends RecyclerView.Adapter<Adapter.Holder> {
+    private Context mContext;
+    ArrayList<CurrencyData> list;
+
+    public Adapter(Context context, ArrayList<CurrencyData> list) {
+        mContext = context;
+        this.list = list;
+    }
+
+    @Override
+    public Holder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        View view = inflater.inflate(R.layout.single_item, parent, false);
+        return new Holder(view);
+    }
+
+
+    @Override
+    public void onBindViewHolder(Holder holder, final int position) {
+        if (list == null || list.size() == 0)
+            return;
+        double val = (Double) list.get(position).getValue();
+        int res = (Integer) list.get(position).getPic();
+        holder.textView.setText(String.valueOf(val));
+        holder.mImageView.setImageResource(res);
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    class Holder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private TextView textView;
+        private ImageView mImageView;
+
+        public Holder(View itemView) {
+            super(itemView);
+            itemView.setOnClickListener(this);
+            textView = (TextView) itemView.findViewById(R.id.result);
+            mImageView = (ImageView) itemView.findViewById(R.id.single_item_imageview);
+        }
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+}
