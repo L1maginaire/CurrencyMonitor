12e4d76a9be01e21a838f9b514434c3b27783d3f
l1maginaire <l1maginaire00@gmail.com>
2018-01-06 16:31:18 +0200

    data_almost_done

--- app/src/main/java/com/example/currencymonitor/MultiChartFragment.java
+++ app/src/main/java/com/example/currencymonitor/MultiChartFragment.java
@@ -2,17 +2,20 @@ package com.example.currencymonitor;
 
 import android.content.Context;
 import android.graphics.Color;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.example.currencymonitor.data.FixerAPI;
+import com.example.currencymonitor.data.MetaCurr;
+import com.example.currencymonitor.di.components.CurrencyComponent;
+import com.example.currencymonitor.di.components.DaggerCurrencyComponent;
+import com.example.currencymonitor.di.modules.ContextModule;
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.XAxis.XAxisPosition;
@@ -20,23 +23,33 @@ import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 import android.support.v4.app.Fragment;
+
+import io.reactivex.Single;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
 /**
  * Created by l1maginaire on 1/6/18.
  */
 
 public class MultiChartFragment extends Fragment {
-
-//    protected Typeface mTfRegular;
-//    protected Typeface mTfLight;
-
+    private static final DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+    private ArrayList<Float> floats = new ArrayList<>();
 
     @Nullable
     @Override
@@ -44,9 +57,9 @@ public class MultiChartFragment extends Fragment {
         View v = inflater.inflate(R.layout.listview_chart, container, false);
 
         ArrayList<BarData> list = new ArrayList<>();
-
+        daysSequence();
         for (int i = 0; i < 10; i++) {
-            list.add(generateData(i + 1));
+            list.add(generateData());
         }
 
         ChartDataAdapter cda = new ChartDataAdapter(getActivity().getApplicationContext(), list);
@@ -83,33 +96,24 @@ public class MultiChartFragment extends Fragment {
                 holder = (ViewHolder) convertView.getTag();
             }
 
-            // apply styling
-//            data.setValueTypeface(mTfLight);
             data.setValueTextColor(Color.BLACK);
             holder.chart.getDescription().setEnabled(false);
             holder.chart.setDrawGridBackground(false);
 
             XAxis xAxis = holder.chart.getXAxis();
             xAxis.setPosition(XAxisPosition.BOTTOM);
-//            xAxis.setTypeface(mTfLight);
             xAxis.setDrawGridLines(false);
 
             YAxis leftAxis = holder.chart.getAxisLeft();
-//            leftAxis.setTypeface(mTfLight);
             leftAxis.setLabelCount(5, false);
             leftAxis.setSpaceTop(15f);
 
             YAxis rightAxis = holder.chart.getAxisRight();
-//            rightAxis.setTypeface(mTfLight);
             rightAxis.setLabelCount(5, false);
             rightAxis.setSpaceTop(15f);
 
-            // set data
             holder.chart.setData(data);
             holder.chart.setFitBars(true);
-
-            // do not forget to refresh the chart
-//            holder.chart.invalidate();
             holder.chart.animateY(700);
 
             return convertView;
@@ -121,28 +125,69 @@ public class MultiChartFragment extends Fragment {
         }
     }
 
-    /**
-     * generates a random ChartData object with just one DataSet
-     *
-     * @return
-     */
-    private BarData generateData(int cnt) {
+    private BarData generateData() {
 
-        ArrayList<BarEntry> entries = new ArrayList<BarEntry>();
+        ArrayList<BarEntry> entries = new ArrayList<>();
+
+//        for (int i = 0; i < floats.size(); i++) {
+//            float val = floats.get(i);
+//            entries.add(new BarEntry(i, val));
+//        }
 
         for (int i = 0; i < 12; i++) {
             entries.add(new BarEntry(i, (float) (Math.random() * 70) + 30));
         }
 
-        BarDataSet d = new BarDataSet(entries, "New DataSet " + cnt);
+        BarDataSet d = new BarDataSet(entries, "New DataSet ");
         d.setColors(ColorTemplate.VORDIPLOM_COLORS);
         d.setBarShadowColor(Color.rgb(203, 203, 203));
 
-        ArrayList<IBarDataSet> sets = new ArrayList<IBarDataSet>();
+        ArrayList<IBarDataSet> sets = new ArrayList<>();
         sets.add(d);
 
         BarData cd = new BarData(sets);
         cd.setBarWidth(0.9f);
         return cd;
+    }
+
+    private void daysSequence() {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        ArrayList<Single<MetaCurr>> dataList = new ArrayList();
+        Calendar calendar = new GregorianCalendar();
+
+        CurrencyComponent daggerRandomUserComponent = DaggerCurrencyComponent.builder()
+                .contextModule(new ContextModule(getContext()))
+                .build();
+        FixerAPI fixerAPI = daggerRandomUserComponent.getCurrencyService();
+
+        for (int i = 0; i < 10; i++) {
+            Date result = calendar.getTime();
+            dataList.add(fixerAPI.statistics(sdf.format(result)));
+            calendar.add(Calendar.DATE, -1);
+        }
+
+        ArrayList <MetaCurr> emptyList = new ArrayList<>();
+
+        Single<List<MetaCurr>> n = Single.merge(dataList).buffer(Integer.MAX_VALUE).single(emptyList);
+
+        CompositeDisposable mCompositeDisposable = new CompositeDisposable();
+
+        mCompositeDisposable.add(n
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<List<MetaCurr>>() {
+                    @Override
+                    public void accept(@NonNull final List<MetaCurr> list) throws Exception {
+                        for (MetaCurr m:list) {
+                            double d = m.getRates().getAUD();
+                            Float f=(float)d;
+                            floats.add(f);
+                        }
+                        generateData();
+                    }
+                })
+        );
+
     }
 }
