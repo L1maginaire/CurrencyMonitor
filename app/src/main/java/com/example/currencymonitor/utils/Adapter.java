a8c454339b9109d40e43e98087d7075a61f4b1cd
l1maginaire <l1maginaire00@gmail.com>
2018-01-25 09:15:36 +0200

    adapter_as_separate_class

--- app/src/main/java/com/example/currencymonitor/utils/Adapter.java
+++ app/src/main/java/com/example/currencymonitor/utils/Adapter.java
@@ -1,6 +1,5 @@
 package com.example.currencymonitor.utils;
 
-import android.content.Intent;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
